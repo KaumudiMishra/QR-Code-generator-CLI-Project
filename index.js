1b8c5be5408b6b5e6ffe88d8770d44233036c2e1
Kaumudi Mishra <kaumudi1204@gmail.com>
2025-10-18 07:06:26 +0530

    Project Done

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
+import express from "express";
+const app = express();
+const port = 3000;
 
 inquirer
+    //input
     .prompt([
         {
             message: "Type your URL: ",
             name: "URL",
         }
     ])
+
     .then((answers) => {
 
+        //qr code generation 
+        const url = answers.URL;
         console.log(answers);
         var qr_svg = qr.image(url);
         qr_svg.pipe(fs.createWriteStream('qr_img.png'));
@@ -23,11 +25,26 @@ inquirer
             if (err) throw err;
             console.log("The file has been saved!");
         });
-    })
+
+        //express
+        app.use(express.static("."));
+        app.get("/", (req, res) => {
+            res.send('<h1>QR image </h1><br><img src="/qr_img.png" height=100px width=100px>');
+        });
+        app.listen(3000, () => {
+            console.log(`server running on port ${port}`);
+        });
+
+    }) 
+
     .catch((error) => {
+
         if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
+            console.log("Prompt couldn't be rendered in this environment");
+        } 
+        
+        else {
+            console.log("Something went wrong:", error);
         }
+
     });
